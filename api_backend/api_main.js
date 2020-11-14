942f9b7ee221837124f1c18a1cd8ec48bfe17901
Rasmus Vekkeli <rasmusj.vekkeli@outlook.comm>
2020-11-14 13:42:21 +0200

    Made express server authenticate using JWTs

--- api_backend/api_main.js
+++ api_backend/api_main.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const jwt = require("express-jwt");
 
 const app = express();
 
-// TODO: Move this into some config file
+// TODO: Move these into some config file
 const port = 80;
+const secret = "Cool Beans";
+
+// Middleware for authenticating JWTs 
+app.use(jwt({ secret: secret, algorithms: ["HS256"] }));
+
+app.use(function (err, req, res, next) {
+	// Handle invalid tokens
+	if (err.name === "UnauthorizedError") {
+		// Send back error info as JSON
+		res.status(401).json(
+			{
+				status: 401,
+				error: "Invalid token"
+			}
+		);
+		
+		return;
+	}
+
+	next();
+});
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
