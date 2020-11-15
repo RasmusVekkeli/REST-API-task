1af0cb6c7a6d8b3e8633470e09787b6a67c8cd33
Rasmus Vekkeli <rasmusj.vekkeli@outlook.comm>
2020-11-15 19:07:13 +0200

    Implemented OMDb API functionality

--- api_backend/api_main.js
+++ api_backend/api_main.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const jwt = require("express-jwt");
 
 // Load config for the backend
@@ -16,7 +17,6 @@ app.use(function (err, req, res, next) {
 		res.status(err.status).json(
 			{
 				status: err.status,
-				name: err.name,
 				message: err.message
 			}
 		);
@@ -27,8 +27,40 @@ app.use(function (err, req, res, next) {
 	next();
 });
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+// Gets movie from OMDb API
+app.get("/getMovie", async (req, res) => {
+	// Check if title is set, send error message if it's not
+	if (req.query.title === undefined) {
+		res.json(
+			{
+				status: res.statusCode,
+				message: "Query parameter \"title\" must be set!"
+			}
+		);
+
+		return;
+	}
+
+	let apiQuery = `http://www.omdbapi.com/?apikey=${config.omdbKey}&t=${req.query.title}&`;
+
+	// Add year to query if it's provided
+	if (req.query.year !== undefined) {
+		apiQuery += `y=${req.query.year}&`;
+	}
+
+	// Add plot type to query if it's provided
+	if (req.query.plot !== undefined) {
+		apiQuery += `plot=${req.query.plot}&`;
+	}
+
+	// Send resulting information back as JSON
+	res.json(
+		// This "await" waits for the Response.json() function because for some reason the function which parses the response body as JSON has to be asynchronous
+		await (
+			// Query the OMDb API
+			await fetch(apiQuery)
+		).json()
+	);
 });
 
 app.listen(config.port, () => {
